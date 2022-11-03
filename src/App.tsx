4a723af13d2c89a1b4d0d30ef0f406fac1b4a512
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-03 13:36:54 +0100

    fragment problems solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import axios from 'axios';
 
@@ -8,6 +8,8 @@ interface INoun {
 	article: string;
 	singular: string;
 	plural: string;
+	isOpen: boolean;
+	isLearned: boolean;
 }
 
 function App() {
@@ -16,10 +18,24 @@ function App() {
 	useEffect(() => {
 		(async () => {
 			const response = await axios.get(nounsUrl);
-			const _nouns = response.data;
+			const rawNouns = response.data;
+			const _nouns: INoun[] = [];
+			rawNouns.forEach((rawNoun: any) => {
+				const _noun: INoun = {
+					...rawNoun,
+					isOpen: false,
+					isLearned: false,
+				};
+				_nouns.push(_noun);
+			});
 			setNouns(_nouns);
 		})();
 	}, []);
+
+	const handleToggleFlashcard = (noun: INoun) => {
+		noun.isOpen = !noun.isOpen;
+		setNouns([...nouns]);
+	};
 
 	return (
 		<div className="App">
@@ -28,9 +44,31 @@ function App() {
 			<div className="nouns">
 				{nouns.map((noun) => {
 					return (
-						<div className="noun" key={noun.singular}>
-							{noun.singular}
-						</div>
+						<React.Fragment key={noun.singular}>
+							{!noun.isLearned && (
+								<div className="noun">
+									<div
+										className="front"
+										onClick={() =>
+											handleToggleFlashcard(noun)
+										}
+									>
+										{noun.singular}
+									</div>
+									{noun.isOpen && (
+										<div className="back">
+											<div className="singular">
+												{noun.article} {noun.singular}
+											</div>
+											<div className="plural">
+												{noun.plural}
+											</div>
+											<button>Mark as learned</button>
+										</div>
+									)}
+								</div>
+							)}
+						</React.Fragment>
 					);
 				})}
 			</div>
